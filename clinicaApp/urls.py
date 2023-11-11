419ffe9df60e0d702b4537a8e39a0ecd62d36b47
leonardoGodoySanchez <l.f.godoy.s@gmail.com>
2023-11-11 19:28:22 -0300

    cambios 11-11-2023 se agregar el menu, cada menu lleva a un apartado de crud

--- clinicaApp/urls.py
+++ clinicaApp/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.clinicaKill),
-    path('about/', views.about)   
+    path('Clinica Kill Coronavirus', views.clinicaKill, name='clinica'),
+    path('about/', views.about),  
+    path('Crear Paciente', views.crearPaciente, name='crearPaciente'),
+    path('Eliminar Paciente', views.eliminarPaciente, name='borrarPaciente'),
+    path('Listar Paciente', views.listarPaciente, name='listarPaciente'),
+    path('Actualizar Paciente', views.actualizarPaciente, name='actualizarPaciente'),
 ]
--- clinicaApp/views.py
+++ clinicaApp/views.py
@@ -3,7 +3,20 @@ from django.http import HttpResponse
 
 # Create your views here.
 def clinicaKill(request):
-    return HttpResponse("<h1>Clínica Kill Coronavirus</h1>")
+    return render(request,"clinicaweb.html")
 
 def about(request):
     return HttpResponse('About')    
+
+def crearPaciente(request):
+    return render(request, "paci_nuevo.html")
+
+def eliminarPaciente(request):
+    return render(request, "paci_borrar.html")
+
+def listarPaciente(request):
+    return render(request, "paci_listar.html")
+
+def actualizarPaciente(request):
+    return render(request, "paci_actualizar.html")
+
