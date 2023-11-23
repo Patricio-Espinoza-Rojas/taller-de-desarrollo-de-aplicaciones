d0a66438d9c4298e391f16b88e91173e863d8296
Patricio Espinoza <p.alfonsoespinoza@gmail.com>
2023-11-23 12:11:29 -0300

    creacion de login

--- OneDrive/Escritorio/taller-de-desarrollo-de-aplicaciones-main/clinicaApp/urls.py
+++ OneDrive/Escritorio/taller-de-desarrollo-de-aplicaciones-main/clinicaApp/urls.py
@@ -1,15 +1,14 @@
 from django.urls import path
-from . import views
+from . import views 
 
 
 urlpatterns = [
     path('', views.clinicaKill, name='clinica'),
-    path('about/', views.about),
+    path('about/', views.about),  
     path('Crear Paciente', views.crearPaciente, name='crearPaciente'),
     path('Eliminar Paciente', views.eliminarPaciente, name='eliminarPaciente'),
     path('Listar Paciente', views.listarPaciente, name='listarPaciente'),
-    path('Actualizar Paciente', views.actualizarPaciente,
-         name='actualizarPaciente'),
+    path('Actualizar Paciente', views.actualizarPaciente, name='actualizarPaciente'),
     path('pacientes/guardar', views.guardar, name='guardar'),
     path('pacientes/eliminar/<int:id>', views.eliminar, name='eliminar'),
     path('pacientes/detalle/<int:id>', views.detalle, name='detalle'),
--- OneDrive/Escritorio/taller-de-desarrollo-de-aplicaciones-main/clinicaApp/views.py
+++ OneDrive/Escritorio/taller-de-desarrollo-de-aplicaciones-main/clinicaApp/views.py
@@ -1,49 +1,38 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.http import HttpResponse 
 from clinicaApp.models import Paciente
 from django.contrib import messages
 
 # Create your views here.
-
-
 def clinicaKill(request):
-    return render(request, "clinicaweb.html")
-
+    return render(request,"clinicaweb.html")
 
 def inicioSesion(request):
     return render(request, "login.html")
 
-
 def about(request):
-    return HttpResponse('About')
-
+    return HttpResponse('About')    
 
 def crearPaciente(request):
     return render(request, "paci_nuevo.html")
 
-
 def eliminarPaciente(request):
-    # pacientes es variable que guarda la consulta de la tabla paciente
-    pacientes = Paciente.objects.all()
-    return render(request, "paci_borrar.html", {
-        'pacientes': pacientes
+    pacientes = Paciente.objects.all() #pacientes es variable que guarda la consulta de la tabla paciente
+    return render(request, "paci_borrar.html",{
+    'pacientes' : pacientes
     })
-
 
 def listarPaciente(request):
-    # pacientes es variable que guarda la consulta de la tabla paciente
-    pacientes = Paciente.objects.all()
-    return render(request, "paci_listar.html", {  # creaciond e diccionarios para enviar los datos al template html que corresponde
-        'pacientes': pacientes
+    pacientes = Paciente.objects.all() #pacientes es variable que guarda la consulta de la tabla paciente
+    return render(request, "paci_listar.html",{ #creaciond e diccionarios para enviar los datos al template html que corresponde
+        'pacientes' : pacientes
     })
-
 
 def actualizarPaciente(request):
     pacientes = Paciente.objects.all()
-    return render(request, "paci_actualizar.html", {
-        'pacientes': pacientes
+    return render(request, "paci_actualizar.html",{
+        'pacientes' : pacientes
     })
-
 
 def guardar(request):
     rut = request.POST["rut"]
@@ -51,12 +40,11 @@ def guardar(request):
     direccion = request.POST["direccion"]
     telefono = request.POST["telefono"]
     correo = request.POST["correo"]
-    pa = Paciente(rut_paciente=rut, nombre_paciente=nombre, direccion_paciente=direccion, telefono_paciente=telefono,
+    pa = Paciente(rut_paciente=rut, nombre_paciente=nombre, direccion_paciente=direccion,telefono_paciente=telefono,
                   correo_paciente=correo)
     pa.save()
     messages.success(request, 'Paciente Guardado')
     return redirect('listarPaciente')
-
 
 def eliminar(request, id):
     paciente = Paciente.objects.filter(pk=id)
@@ -64,13 +52,11 @@ def eliminar(request, id):
     messages.success(request, 'Paciente eliminado')
     return redirect('listarPaciente')
 
-
 def detalle(request, id):
     paciente = Paciente.objects.get(pk=id)
-    return render(request, "paciente_editar.html", {
-        'paciente': paciente
+    return render(request, "paciente_editar.html",{
+        'paciente' : paciente
     })
-
 
 def editar(request):
     rut = request.POST["rut"]
@@ -79,7 +65,13 @@ def editar(request):
     telefono = request.POST["telefono"]
     correo = request.POST["correo"]
     id = request.POST["id"]
-    Paciente.objects.filter(pk=id).update(rut_paciente=rut, nombre_paciente=nombre, direccion_paciente=direccion_paciente, telefono_paciente=telefono,
-                                          correo_paciente=correo)
+    Paciente.objects.filter(pk=id).update(rut_paciente=rut, nombre_paciente=nombre, direccion_paciente=direccion_paciente,telefono_paciente=telefono,
+                  correo_paciente=correo)
     messages.success(request, 'Paciente Actualizado')
     return redirect('listarPaciente')
+
+    
+    
+    
+
+
